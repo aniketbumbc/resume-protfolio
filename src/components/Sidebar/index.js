c70f72224af983bd1ea45f783f1aa566a4f93d3f
aniket <aniket.umbc@gmail.com>
2022-06-24 13:11:41 +0530

    Feat: side nav bar complete

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,6 +3,7 @@ import './index.scss';
 import LogoBunny from '../../assets/images/logo.png';
 import LogoName from '../../assets/images/logo_name.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import {
   faEnvelope,
   faHome,
@@ -49,6 +50,18 @@ const Sidebar = () => {
             <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
           </NavLink>
         </nav>
+        <ul>
+          <li>
+            <a target="_blank" rel="noreferrer" href="">
+              <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreferrer" href="">
+              <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+            </a>
+          </li>
+        </ul>
       </div>
     </>
   );
