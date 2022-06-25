febf943426a91740b103ae9585cd93e279a3ddbb
aniket <aniket.umbc@gmail.com>
2022-06-25 12:41:31 +0530

    Feat: Animated added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,45 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo.png';
+import { jobArray, nameArray } from '../../constant';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+  }, []);
+
   return (
     <>
       <div className="container home-page">
         <div className="text-zone">
-          <h1 style={{ marginLeft: '500px' }}>
-            Hi, <br /> I'm{' '}
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={25}
+            />
           </h1>
-          <img src={LogoTitle} alt="developer" />
+          <h2>Fullstack Developer / Javascript Developer</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
         </div>
       </div>
     </>
