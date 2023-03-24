e7780fc8cb2d5588128f48794c2296623b9ea339
aniket <aniket.umbc@gmail.com>
2023-03-24 07:26:56 +0530

    styling componenet

--- src/components/Code/index.js
+++ src/components/Code/index.js
@@ -1,56 +1,234 @@
 import './index.scss';
 import demoImage from '../../assets/images/react.jpeg';
+import { faGithub, faReact } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ShowCodeSkill = () => {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}> Work In Progress........</h1>
       <div className="card-container">
-        <div className="card">
+        <div
+          className="card"
+          onClick={() => window.open('https://github.com/aniketbumbc')}
+        >
           <div className="image">
             <img src={demoImage} alt="demoImage" />
           </div>
 
           <div className="details">
-            <div className="center ">
+            <div className="center">
               <h1>
-                Someone famous
-                <br />
-                <span>team leader</span>
+                Someone famous{' '}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/aniketbumbc"
+                >
+                  <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
               </h1>
-
-              <p>
+              <span>React, Typescript, Scss, Redux</span>
+              {/* <p>
                 Lorem ipsum is simple dummy text on the printing and typesetting
                 industry.
-              </p>
-              <p>
-                Lorem ipsum is simple dummy text on the printing and typesetting
-                industry.
-              </p>
-              <p>
-                Lorem ipsum is simple dummy text on the printing and typesetting
-                industry.
-              </p>
+              </p> */}
             </div>
           </div>
         </div>
-
-        <div className="card">
+        <div
+          className="card"
+          onClick={() => window.open('https://github.com/aniketbumbc')}
+        >
           <div className="image">
             <img src={demoImage} alt="demoImage" />
           </div>
-          <div className="details">
-            <div className="center ">
-              <h1>
-                Someone famous
-                <br />
-                <span>team leader</span>
-              </h1>
 
-              <p>
+          <div className="details">
+            <div className="center">
+              <h1>
+                Someone famous{' '}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/aniketbumbc"
+                >
+                  <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+              </h1>
+              <span>React, Typescript, Scss, Redux</span>
+              {/* <p>
                 Lorem ipsum is simple dummy text on the printing and typesetting
                 industry.
-              </p>
+              </p> */}
+            </div>
+          </div>
+        </div>
+        <div
+          className="card"
+          onClick={() => window.open('https://github.com/aniketbumbc')}
+        >
+          <div className="image">
+            <img src={demoImage} alt="demoImage" />
+          </div>
+
+          <div className="details">
+            <div className="center">
+              <h1>
+                Someone famous{' '}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/aniketbumbc"
+                >
+                  <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+              </h1>
+              <span>React, Typescript, Scss, Redux</span>
+              {/* <p>
+                Lorem ipsum is simple dummy text on the printing and typesetting
+                industry.
+              </p> */}
+            </div>
+          </div>
+        </div>
+        <div
+          className="card"
+          onClick={() => window.open('https://github.com/aniketbumbc')}
+        >
+          <div className="image">
+            <img src={demoImage} alt="demoImage" />
+          </div>
+
+          <div className="details">
+            <div className="center">
+              <h1>
+                Someone famous{' '}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/aniketbumbc"
+                >
+                  <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+              </h1>
+              <span>React, Typescript, Scss, Redux</span>
+              {/* <p>
+                Lorem ipsum is simple dummy text on the printing and typesetting
+                industry.
+              </p> */}
+            </div>
+          </div>
+        </div>
+        <div
+          className="card"
+          onClick={() => window.open('https://github.com/aniketbumbc')}
+        >
+          <div className="image">
+            <img src={demoImage} alt="demoImage" />
+          </div>
+
+          <div className="details">
+            <div className="center">
+              <h1>
+                Someone famous{' '}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/aniketbumbc"
+                >
+                  <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+              </h1>
+              <span>React, Typescript, Scss, Redux</span>
+              {/* <p>
+                Lorem ipsum is simple dummy text on the printing and typesetting
+                industry.
+              </p> */}
+            </div>
+          </div>
+        </div>
+        <div
+          className="card"
+          onClick={() => window.open('https://github.com/aniketbumbc')}
+        >
+          <div className="image">
+            <img src={demoImage} alt="demoImage" />
+          </div>
+
+          <div className="details">
+            <div className="center">
+              <h1>
+                Someone famous{' '}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/aniketbumbc"
+                >
+                  <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+              </h1>
+              <span>React, Typescript, Scss, Redux</span>
+              {/* <p>
+                Lorem ipsum is simple dummy text on the printing and typesetting
+                industry.
+              </p> */}
+            </div>
+          </div>
+        </div>
+        <div
+          className="card"
+          onClick={() => window.open('https://github.com/aniketbumbc')}
+        >
+          <div className="image">
+            <img src={demoImage} alt="demoImage" />
+          </div>
+
+          <div className="details">
+            <div className="center">
+              <h1>
+                Someone famous{' '}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/aniketbumbc"
+                >
+                  <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+              </h1>
+              <span>React, Typescript, Scss, Redux</span>
+              {/* <p>
+                Lorem ipsum is simple dummy text on the printing and typesetting
+                industry.
+              </p> */}
+            </div>
+          </div>
+        </div>{' '}
+        <div
+          className="card"
+          onClick={() => window.open('https://github.com/aniketbumbc')}
+        >
+          <div className="image">
+            <img src={demoImage} alt="demoImage" />
+          </div>
+
+          <div className="details">
+            <div className="center">
+              <h1>
+                Someone famous{' '}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/aniketbumbc"
+                >
+                  <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+              </h1>
+              <span>React, Typescript, Scss, Redux</span>
+              {/* <p>
+                Lorem ipsum is simple dummy text on the printing and typesetting
+                industry.
+              </p> */}
             </div>
           </div>
         </div>
@@ -58,142 +236,38 @@ const ShowCodeSkill = () => {
           <div className="image">
             <img src={demoImage} alt="demoImage" />
           </div>
-          <div className="details">
-            <div className="center ">
-              <h1>
-                Someone famous
-                <br />
-                <span>team leader</span>
-              </h1>
 
-              <p>
+          <div className="details">
+            <div className="center">
+              <h1>Someone famous Test </h1>
+              <span>React, Typescript, Scss, Redux </span>
+              <h1>
+                <a
+                  className="code-icon"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://welearncode.com/create-a-card-layout/"
+                >
+                  <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+                <a
+                  className="code-icon"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://welearncode.com/create-a-card-layout/"
+                >
+                  <FontAwesomeIcon icon={faReact} color="#4d4d4e" />
+                </a>
+              </h1>
+              {/* <p>
                 Lorem ipsum is simple dummy text on the printing and typesetting
                 industry.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="image">
-            <img src={demoImage} alt="demoImage" />
-          </div>
-          <div className="details">
-            <div className="center ">
-              <h1>
-                Someone famous
-                <br />
-                <span>team leader</span>
-              </h1>
-
-              <p>
-                Lorem ipsum is simple dummy text on the printing and typesetting
-                industry.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="image">
-            <img src={demoImage} alt="demoImage" />
-          </div>
-          <div className="details">
-            <div className="center ">
-              <h1>
-                Someone famous
-                <br />
-                <span>team leader</span>
-              </h1>
-
-              <p>
-                Lorem ipsum is simple dummy text on the printing and typesetting
-                industry.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="image">
-            <img src={demoImage} alt="demoImage" />
-          </div>
-          <div className="details">
-            <div className="center ">
-              <h1>
-                Someone famous
-                <br />
-                <span>team leader</span>
-              </h1>
-
-              <p>
-                Lorem ipsum is simple dummy text on the printing and typesetting
-                industry.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="image">
-            <img src={demoImage} alt="demoImage" />
-          </div>
-          <div className="details">
-            <div className="center ">
-              <h1>
-                Someone famous
-                <br />
-                <span>team leader</span>
-              </h1>
-
-              <p>
-                Lorem ipsum is simple dummy text on the printing and typesetting
-                industry.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="image">
-            <img src={demoImage} alt="demoImage" />
-          </div>
-          <div className="details">
-            <div className="center ">
-              <h1>
-                Someone famous
-                <br />
-                <span>team leader</span>
-              </h1>
-
-              <p>
-                Lorem ipsum is simple dummy text on the printing and typesetting
-                industry.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="image">
-            <img src={demoImage} alt="demoImage" />
-          </div>
-          <div className="details">
-            <div className="center ">
-              <h1>
-                Someone famous
-                <br />
-                <span>team leader</span>
-              </h1>
-
-              <p>
-                Lorem ipsum is simple dummy text on the printing and typesetting
-                industry.
-              </p>
+              </p> */}
             </div>
           </div>
         </div>
       </div>
+      <div className="test"></div>
     </>
   );
 };
