1f5026bcddba013b0d3a57977fc0858f65f913c1
aniket <aniket.umbc@gmail.com>
2023-03-25 06:11:21 +0530

    Test cases fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  <BrowserRouter>
+    render(
+    <App />
+    );
+  </BrowserRouter>;
+  const element = screen.getByTestId('route-element');
+  expect(element).toBeInTheDocument();
 });
